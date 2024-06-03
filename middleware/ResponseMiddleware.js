af680972b791992b64a283ad61a9dd1e20ad4948
rifqio <oktariorifqi@gmail.com>
2024-06-04 00:12:09 +0700

    feat: sales trend and demand

--- middleware/ResponseMiddleware.js
+++ middleware/ResponseMiddleware.js
@@ -5,66 +5,66 @@ const ResponseMiddleware = (req, res, next) => {
     res.success = (message = null) => {
         return res.status(200).json({
             status: true,
-            message: message || 'Success',
             transaction_id: req.transaction_id,
+            message: message || 'Success',
         });
     };
 
     res.successWithData = (data, message = null) => {
         return res.status(200).json({
             status: true,
+            transaction_id: req.transaction_id,
             message: message || 'Success',
             data: data,
-            transaction_id: req.transaction_id,
         });
     };
 
     res.created = (message = null) => {
         return res.status(201).json({
             status: true,
-            message: message || 'Success',
             transaction_id: req.transaction_id,
+            message: message || 'Success',
         });
     };
 
     res.createdWithData = (data, message = null) => {
         return res.status(201).json({
             status: true,
+            transaction_id: req.transaction_id,
             message: message || 'Success',
             data: data,
-            transaction_id: req.transaction_id,
         });
     };
 
     res.badRequest = (message = null) => {
         return res.status(400).json({
             status: false,
-            message: message || 'Bad Request',
             transaction_id: req.transaction_id,
+            message: message || 'Bad Request',
         });
     };
 
     res.unauthorized = (message = null) => {
         return res.status(401).json({
             status: false,
-            message: message || 'Unauthorized',
             transaction_id: req.transaction_id,
+            message: message || 'Unauthorized',
         });
     };
 
     res.forbidden = (message = null) => {
         return res.status(403).json({
             status: false,
-            message: message || 'Forbidden',
             transaction_id: req.transaction_id,
+            message: message || 'Forbidden',
         });
     };
 
     res.internalServerError = (message = null) => {
         return res.status(500).json({
             status: false,
-            message: message || 'Internal Server Error',
             transaction_id: req.transaction_id,
+            message: message || 'Internal Server Error',
         });
     };
 
--- service/ChartService.js
+++ service/ChartService.js
@@ -1,80 +1,103 @@
-const { TransactionType } = require('../utils/constants');
+const { round } = require('lodash');
 const db = require('../utils/database');
 const moment = require('moment');
+const { TransactionType } = require('../utils/constants');
 
 const GetTotalSales = async ({ startDate, endDate, fishType }) => {
-    let options = {
-        where: {},
+    const filterConditions = {
+        transaction_type: TransactionType.DEDUCT,
     };
 
-    if (options.fishType) {
-        options.where.fish_type = fishType;
-    }
-
-    if (options.startDate && options.endDate) {
-        options.where.created_at = {
-            gte: startDate,
-            lte: endDate,
+    const localStartDate = moment(startDate).add(7, 'hours');
+    const localEndDate = moment(endDate).add(7, 'hours');
+    
+    if (startDate && endDate) {
+        filterConditions.created_at = {
+            gte: localStartDate.toDate(),
+            lte: localEndDate.toDate()
         };
     }
 
-    if (options.startDate) {
-        options.where.created_at = {
-            gte: startDate,
-            lte: moment().toISOString(),
+    if (startDate) {
+        filterConditions.created_at = {
+            gte: localStartDate.toDate(),
         };
     }
 
-    if (options.endDate) {
-        options.where.created_at = {
-            lte: endDate,
+    if (endDate) {
+        filterConditions.created_at = {
+            lte: localEndDate.toDate()
+        };
+    }
+    
+    if (fishType) {
+        filterConditions.FishStock = {
+            fish_type: fishType,
         };
     }
 
-    const baseQuery = await db.fishStock.findMany({
-        select: {
-            fish_type: true,
+    const totalSalesPerFishTypeRaw = await db.fishStockTransaction.groupBy({
+        by: ['fish_stock_id', 'transaction_type'],
+        where: filterConditions,
+        _sum: {
             quantity: true,
-            transactions: {
-                select: {
-                    quantity: true,
-                    transaction_type: true,
-                    created_at: true,
-                },
-            },
         },
-        ...options
     });
 
-    const fishSales = {};
+    const totalSalesPerFishType = await Promise.all(
+        totalSalesPerFishTypeRaw.map(async (sale) => {
+            const fishStock = await db.fishStock.findUnique({
+                where: { id_fish_stock: sale.fish_stock_id },
+                include: {
+                    fish: true,
+                    warehouse: true,
+                },
+            });
 
-    baseQuery.forEach((stock) => {
-        const { fish_type, quantity, transactions } = stock;
-        if (!fishSales[fish_type]) {
-            fishSales[fish_type] = {
-                stock_in: 0,
-                stock_out: 0,
-                current_stock: quantity,
+            return {
+                fish_type: fishStock.fish.type,
+                warehouse_id: fishStock.warehouse_id,
+                transaction_type: sale.transaction_type,
+                total_quantity_sold: sale._sum.quantity,
             };
-        }
+        })
+    );
 
-        transactions.forEach((transaction) => {
-            if (transaction.transaction_type === TransactionType.ADD) {
-                fishSales[fish_type].stock_in += transaction.quantity;
-            } else if (transaction.transaction_type === TransactionType.DEDUCT) {
-                fishSales[fish_type].stock_out += transaction.quantity;
-            }
-        });
+    const salesOverTimeRaw = await db.fishStockTransaction.groupBy({
+        by: ['created_at'],
+        where: filterConditions,
+        _sum: {
+            quantity: true,
+        },
+        orderBy: {
+            created_at: 'asc',
+        },
     });
 
-    const totalSales = Object.keys(fishSales).map((fish_type) => ({
-        fish_type,
-        ...fishSales[fish_type],
-    }));
+    const salesOverTimeGrouped = salesOverTimeRaw.reduce((acc, sale) => {
+        const date = moment(sale.created_at).format('YYYY-MM-DD');
+        if (!acc[date]) {
+            acc[date] = { date, total_quantity_sold: 0 };
+        }
+        acc[date].total_quantity_sold += sale._sum.quantity;
+        return acc;
+    }, {});
+
+    const salesOverTime = Object.values(salesOverTimeGrouped);
+
+    const averageSalesPerDay = await db.fishStockTransaction.aggregate({
+        where: filterConditions,
+        _avg: {
+            quantity: true,
+        },
+    });
 
     return {
-        date: moment().format('YYYY-MM-DD'),
-        total_sales: totalSales,
+        totalSalesPerFishType,
+        salesOverTime,
+        averageSalesPerDay: {
+            average_quantity_sold: round(averageSalesPerDay._avg.quantity, 2),
+        },
     };
 };
 
